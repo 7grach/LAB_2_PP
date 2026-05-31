a04fe82500fb98a6739c0677469f3ec138274d76
7grach <grachev.alexandr001@gmail.com>
2026-05-31 14:19:42 +0300

    сделал фичу добавления task

---
+++ task_manager.py
@@ -0,0 +1,16 @@
+from storage import load_tasks, save_tasks
+
+def show_tasks():
+    tasks = load_tasks()
+
+    if not tasks:
+        print("No tasks")
+        return
+
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+
+def add_task(task):
+    tasks = load_tasks()
+    tasks.append(task)
+    save_tasks(tasks)
